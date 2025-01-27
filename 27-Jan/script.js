c6988a21162014d7485f1eecc84615b2d4ecb71b
kartikkkdua <kartik.dua@icloud.com>
2025-01-27 08:37:12 +0530

    updates in 27-Jan

--- 27-Jan/script.js
+++ 27-Jan/script.js
@@ -2,4 +2,24 @@
 console.log("Hello World");
 console.warn("This is a warning");
 console.error("This is an error");
-console.write("This is a write");
+console.info("This is an information");
+
+// Airthmatic Operator : + , - , / , * , %
+// == , === , != , !==
+//Assignment Operator :  > , < , >= , <=
+// Logical Operator :&& , || , !
+// ++ , --
+// += , -= , *= , /= , %=
+//  ? :
+
+//Function in javascript
+function greet(name){
+    console.log("Hello " + name);
+}
+greet("John");
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(2,3));
+
