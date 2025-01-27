fdffb5d8b21775922cc42640a4f07c66488629ac
kartikkkdua <kartik.dua@icloud.com>
2025-01-27 08:22:40 +0530

    27 Jan

---
+++ 27-Jan/script.js
@@ -0,0 +1,5 @@
+// use of log, warn, error, write in javascript. 
+console.log("Hello World");
+console.warn("This is a warning");
+console.error("This is an error");
+console.write("This is a write");
